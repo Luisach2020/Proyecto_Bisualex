83aa01e6bd2ac03fa370c399db76436226686a9a
lu16253-nh <restrepocarvajalluisaf100@gmail.com>
2026-05-04 14:17:16 -0500

    cambio

---
+++ src/componentes/StatusLuisa.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+export const StatusLuisa = () => {
+    const [puntos, setPuntos] = useState(' ');
+    <>
+        <div className='puntos'>
+            <button>100</button>
+            <button>100 *emoji*</button>
+            <button>200 🤗</button>
+            <button>200 ☺️​😂</button>
+
+
+        </div>
+
+
+    </>
+    return (
+
+ <div>StatusLuisa</div>
+
+    )
+}
