ff425b74c5b25b2675026910e15fefa731bd7568
lu16253-nh <restrepocarvajalluisaf100@gmail.com>
2026-05-07 13:31:17 -0500

    rick

--- src/componentes/StatusLuisa.jsx
+++ src/componentes/StatusLuisa.jsx
@@ -2,6 +2,22 @@ import React, { useState } from 'react'
 
 export const StatusLuisa = () => {
     const [puntos, setPuntos] = useState(' ');
+
+
+
+
+     const getCharacters= async()=>{
+        const res= await fetch("https://rickandmortyapi.com/api/character")
+        const data = await res.json()
+        console.log( data )
+
+          useEffect(() => {
+
+   getCharacters()
+
+    }, [])
+
+    }
     <>
         <div className='puntos'>
          <button onClick={() => { setPuntos ( (prev) => {prev + 2} )}}>Puntos</button>
